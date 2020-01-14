e64e73503face850e625e376240e8a903e279b82
Santiago Gomez Florez <58926096+sango09@users.noreply.github.com>
2020-01-13 22:13:45 -0500

    2. Modified row_turn

--- Banplatzi/fila_de_banco.py
+++ Banplatzi/fila_de_banco.py
@@ -6,75 +6,43 @@ row_deposit = deque([])
 row_account = deque([])
 
 # counter of turn
-idx_deposit = 0
-idx_account = 0
+COUNT_DEP = 0
+COUNT_ACC = 0
 
 
-def add_deposit(client_deposit):
-    # Add client to deposit row
-    global idx_deposit
-
-    idx_deposit += 1
-    row_deposit.append('Turno ' + str(idx_deposit))
+def add_row(client_deposit):
     print(' Ha sido agregado a la fila '.center(50, '='))
-
     print('')
     print('Turno | Nombre | Cedula |')
     print('')
-
     print('{uid_deposit} | {name} | {id} |'.format(
-
-        uid_deposit=idx_deposit,
-        name=client_deposit['name'],
-        id=client_deposit['id'],
-
-    ))
-
-
-def add_account(new_account):
-    # Add client to account row
-    global idx_account
-
-    idx_account += 1
-    row_account.append('Turno: ' + str(idx_account))
-    print(' Ha sido agregado a la fila '.center(50, '='))
-
-    print('')
-    print('Turno | Nombre | Cedula |')
-    print('')
-
-    print('{uid_account} | {name} | {id} |'.format(
-
-        uid_account=idx_account,
-        name=new_account['name'],
-        id=new_account['id']
-    ))
+            uid_deposit=client_deposit['turn'],
+            name=client_deposit['name'],
+            id=client_deposit['id'],))
+    run()
 
 
 def serv_client():
     # Attend bank clients
     if row_account:
-        print('Fila de APERTURA DE CUENTAS: {}'.format(
-            row_account[0])
-        )
+        client = row_account.popleft()
+        print('Fila de APERTURA DE CUENTAS: Turno: {} , Cliente: {}'.format(
+            client['turn'], client['name']))
         print('')
-
         print('El cliente debe ser atendido'.center(50, '='))
-        row_account.popleft()
         run()
 
     elif row_deposit:
-        print('Fila de DEPOSITO: {}'.format(
-            row_deposit[0])
-        )
+        client = row_deposit.popleft()
+        print('Fila de DEPOSITO: Turno: {} , Cliente: {}'.format(
+            client['turn'], client['name']))
         print('')
-
         print('El cliente debe ser atendido'.center(50, '='))
-        row_deposit.popleft()
         run()
 
     else:
         print('NO HAY CLIENTES PARA ATENDER'.center(50, '='))
+        print(''.center(50, '*'))
         run()
 
 
@@ -93,78 +61,85 @@ def list_clients():
     print('Fila de APERTURA DE CUENTA tiene: {} clientes'.format(
         len(row_account))
     )
+    run()
 
 
 def _get_client_field(field_name, message='¿Cual es {} del cliente?: '):
     # Get client input
     field = None
-
     while not field:
         field = input(message.format(field_name))
-
     return field
 
 
-def _get_client_from_user():
-    #
+def _get_client_from_user(pas):
     client = {
         'name': _get_client_field('el nombre'),
-        'id': _get_client_field('la cedula')
+        'id': _get_client_field('la cedula'),
+        'turn': assign_shift(pas)
     }
     return client
 
 
+def assign_shift(pas):
+    
+    if pas == 'd':
+        global COUNT_DEP
+        COUNT_DEP += 1
+        return str("DP - " + str(COUNT_DEP))
+    else:
+        global COUNT_ACC
+        COUNT_ACC += 1
+        return str("ACC - " + str(COUNT_ACC))
+
+
 def run():
+    # commands for services
+    command = str(input("""
+        ¿Que deseas hacer hoy?
 
-    while True:
-        # commands for services
-        command = str(input("""
-            ¿Que deseas hacer hoy?
+    [1] Agregar cliente a fila de depositos
+    [2] Agregar cliente a fila de apertura de cuenta
+    [3] Atender cliente
+    [4] Listar clientes en fila
 
-        [1] Agregar cliente a fila de depositos
-        [2] Agregar cliente a fila de apertura de cuenta
-        [3] Atender cliente
-        [4] Listar clientes en fila
+            [S]alir
+    : """))
+    command = command.upper()
 
-                [S]alir
-        : """))
-        command = command.upper()
+    if command == '1':
+        print('FILA PARA DEPÓSITO'.center(50, '='))
+        print('')
+        client_deposit = _get_client_from_user("d")
+        row_deposit.append(client_deposit)
+        add_row(client_deposit)
 
-        if command == '1':
-            print('FILA PARA DEPÓSITO'.center(50, '='))
-            print('')
-            client_deposit = _get_client_from_user()
-            type_row = 'Fila de deposito'
+    elif command == '2':
+        print('FILA PARA APERTURA DE CUENTAS'.center(50, '='))
+        print('')
+        new_account = _get_client_from_user("a")
+        row_account.append(new_account)
+        add_row(new_account)
 
-            add_deposit(client_deposit)
+    elif command == '3':
+        print(' TURNO DEL CLIENTE POR ATENDER'.center(50, '='))
+        print('')
+        serv_client()
 
-        elif command == '2':
-            print('FILA PARA APERTURA DE CUENTAS'.center(50, '='))
-            print('')
-            new_account = _get_client_from_user()
+    elif command == '4':
+        print(' LISTA DE CLIENTES EN LA FILA '.center(50, '='))
+        print('')
+        list_clients()
 
-            add_account(new_account)
+    elif command == 'S':
+        print('¡ Gracias por usar Banplatzi !'.center(50, '-'))
+        return 1
 
-        elif command == '3':
-            print(' TURNO DEL CLIENTE POR ATENDER'.center(50, '='))
-            print('')
-
-            serv_client()
-
-        elif command == '4':
-            print(' LISTA DE CLIENTES EN LA FILA '.center(50, '='))
-            print('')
-            list_clients()
-
-        elif command == 'S':
-            print('! Gracias por usar Banplatzi ¡'.center(50, '-'))
-            break
-
-        else:
-            print('Comando invalido')
+    else:
+        print('Comando invalido')
 
 
 if __name__ == '__main__':
     # Message welcome
-    print('BIENVENIDO A BANPLATZI'.center(50, '='))
+    print(' BIENVENIDO A BANPLATZI '.center(50, '='))
     run()
